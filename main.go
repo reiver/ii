22d6a4188760f787ed59b7f7f99d6fa4765784a2
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-21 08:57:03 -0700

    main.go

--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"github.com/reiver/go-cli"
+
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Intergalactic Index")
+
+	var handler cli.Handler
+
+	cli.RunAndThenExit(handler)
 }
