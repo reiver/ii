66fcc1d2eb0c3f82ebde7a1e0e5bf76530b0e887
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-22 10:10:56 -0700

    colored ii

--- main.go
+++ main.go
@@ -7,12 +7,23 @@ import (
 	"github.com/reiver/go-cli"
 
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Intergalactic Index")
+	fmt.Printf("Intergalactic Index (%s) ⚡\n", attn("ii"))
 
 	var handler cli.Handler = &sys_command.Mux
 
 	cli.RunAndThenExit(handler)
+}
+
+func attn(s string) string {
+	var builder strings.Builder
+
+	builder.WriteString("\x1b[94;40m")
+	builder.WriteString(s)
+	builder.WriteString("\x1b[0m")
+
+	return builder.String()
 }
