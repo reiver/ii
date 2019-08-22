6298ae9fee96b4afeed1f2b64b3e16c8bc35ef8a
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-22 15:07:15 -0700

    ii help

--- commands/help/command.go
+++ commands/help/command.go
@@ -5,8 +5,10 @@ import (
 	"github.com/reiver/ii/sys/uhoh"
 
 	"github.com/reiver/go-cli"
+	"github.com/reiver/go-iirepo/apps"
 
 	"io"
+	"os"
 	"strings"
 )
 
@@ -41,15 +43,64 @@ func run(stdin io.ReadCloser, stdout io.WriteCloser, stderr io.WriteCloser, comm
 			builder.Reset()
 
 			builder.WriteString("\n\tii ")
+			builder.WriteString("\x1b[32m")
 			builder.WriteString(c[0])
+			builder.WriteString("\x1b[0m")
 			for _, t := range c[1:] {
 				builder.WriteRune(' ')
+				builder.WriteString("\x1b[32m")
 				builder.WriteString(t)
+				builder.WriteString("\x1b[0m")
 			}
 			builder.WriteRune('\n')
 
 			io.WriteString(stdout, builder.String())
 		}
+	}
+
+	var iiapps [][]string
+	{
+		wd, err := os.Getwd()
+		if nil != err {
+			sys_uhoh.Fprintf(stderr, "%s\n", err)
+			return cli.ExitCodeError
+		}
+
+		iiapps, err = iirepo_apps.List(wd)
+		if nil != err {
+			sys_uhoh.Fprintf(stderr, "%s\n", err)
+			return cli.ExitCodeError
+		}
+	}
+
+	if 0 < len(iiapps) {
+		io.WriteString(stdout, "\nThese are the installed ii apps:\n")
+
+		var builder strings.Builder
+
+		for _, c := range iiapps {
+			if 0 >= len(c) {
+				continue
+			}
+
+			builder.Reset()
+
+			builder.WriteString("\n\tii ")
+			builder.WriteString("\x1b[32m")
+			builder.WriteString(c[0])
+			builder.WriteString("\x1b[0m")
+			for _, t := range c[1:] {
+				builder.WriteRune(' ')
+				builder.WriteString("\x1b[32m")
+				builder.WriteString(t)
+				builder.WriteString("\x1b[0m")
+			}
+			builder.WriteRune('\n')
+
+			io.WriteString(stdout, builder.String())
+		}
+	} else {
+		io.WriteString(stdout, "\nThere are no installed ii apps for this repo.\n")
 	}
 
 	return cli.ExitCodeOK
